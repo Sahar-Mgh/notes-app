fbc3897a94fb4ef884aca5dec804881785624428
Sahar-Mgh <sahar.emma.m@gmail.com>
2024-01-09 16:41:16 +0330

    added local storage codes to store the codes in the web browser

--- script.js
+++ script.js
@@ -2,7 +2,13 @@ const notesContainer = document.querySelector(".notes-container");
 const createBtn = document.querySelector(".btn");
 let notes = document.querySelector(".input-box");
 
-
+function showNotes(){
+    notesContainer.innerHTML = localStorage.getItem("notes");
+}
+showNotes();
+function updateStorage(){
+    localStorage.setItem("notes",notesContainer.innerHTML);
+}
 
 createBtn.addEventListener("click",()=>{
     let inputBox=document.createElement("p");
