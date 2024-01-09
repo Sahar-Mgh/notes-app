07e19126341cec3b35ad78b812c14e99928fb8f1
Sahar-Mgh <sahar.emma.m@gmail.com>
2024-01-09 16:48:21 +0330

    added Enter event listener to prevent default feature

--- script.js
+++ script.js
@@ -32,3 +32,9 @@ notesContainer.addEventListener("click",function(e){
         });
     }
 });
+document.addEventListener("keydown",event =>{
+    if(event.key==="Enter"){
+        document.execCommand("insertLineBreak");
+        event.preventDefault();
+    }
+});
